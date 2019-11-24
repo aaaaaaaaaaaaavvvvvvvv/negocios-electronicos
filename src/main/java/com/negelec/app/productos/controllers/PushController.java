b709aa6f4fa93a64abb11589dfe309f3bafb3aa6
Eduardo <lalorhp@gmail.com>
2019-11-24 18:08:06 -0500

    Agregando modelo para el push

--- src/main/java/com/negelec/app/productos/controllers/PushController.java
+++ src/main/java/com/negelec/app/productos/controllers/PushController.java
@@ -15,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 import com.negelec.app.productos.model.CuerpoNotificacion;
 import com.negelec.app.productos.model.DatosNotificaciones;
 import com.negelec.app.productos.model.PushMessageModel;
+import com.negelec.app.productos.model.PushNotificationModel;
 
 @RestController
 public class PushController {
@@ -23,20 +24,21 @@ public class PushController {
 	RestTemplate restTemplate;
 
 	@PostMapping("/push-notification")
-	public void sendPushNotification(@RequestBody String usuario) {
+	public String sendPushNotification(@RequestBody PushNotificationModel push) {
 		PushMessageModel pmm = new PushMessageModel();
 		CuerpoNotificacion cnt = new CuerpoNotificacion();
 		DatosNotificaciones dtn = new DatosNotificaciones();
-		pmm.setTo("dW1PxJXYy7o:APA91bHOh_BBQ6ZzzPmtBpr2xowDoG-6YYint4tfyHqU_voCodBPmpzF2TSnOW5rp3my_dFn_w1PoeF_sN6GcQvbDomJV8FYGQJOMXAxj43fGH-uO3QsCccrhP2mcdxLsbdiYXpXyKtm");
+		pmm.setTo(push.getCodCelular());
 		cnt.setBody("Se concreto una compra desde nuestra web");
 		cnt.setTitle("Nueva Compra");
-		dtn.setCodigoUsuario(usuario);
+		dtn.setCodigoUsuario(push.getUsuario());
 		pmm.setNotification(cnt);
 		pmm.setData(dtn);
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Authorization", "key=AAAALnwfbrQ:APA91bHQoPg8HO05adg6t42y_yUwdI6e0J9uKm1CeGRFZUCwECKuitu-6BCmPVPldtm_QjP_rAQpxcWUfauLgSm22BIgrokwVLE_QUPTvzRx0sToxFE68z5k2c8MBCwNBFJ3EfFXe-ON");
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<PushMessageModel> entity = new HttpEntity<PushMessageModel>(pmm, headers);
-		restTemplate.exchange("https://fcm.googleapis.com/fcm/send", HttpMethod.POST, entity, String.class).getBody();
+		System.out.println(entity);
+		return restTemplate.exchange("https://fcm.googleapis.com/fcm/send", HttpMethod.POST, entity, String.class).getBody();
 	}
 }
---
+++ src/main/java/com/negelec/app/productos/model/PushNotificationModel.java
@@ -0,0 +1,23 @@
+package com.negelec.app.productos.model;
+
+public class PushNotificationModel {
+	private String usuario;
+	private String codCelular;
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getCodCelular() {
+		return codCelular;
+	}
+
+	public void setCodCelular(String codCelular) {
+		this.codCelular = codCelular;
+	}
+
+}
